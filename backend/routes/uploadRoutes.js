8add462ba513c322bb4e93c2af8410259dece479
Roni Ganrai <137520759+roniganrai@users.noreply.github.com>
2025-06-20 21:20:13 +0530

    Revert " Backend core features completed: Auth + Excel upload"

--- backend/routes/uploadRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { uploadExcel } = require("../controllers/uploadController");
-
-router.post("/upload", uploadExcel);
-
-module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
-const authRoutes = require("./routes/authRoutes");
 const cors = require("cors");
-const uploadRoutes = require("./routes/uploadRoutes");
+const connectDB = require("./config/db");
 
 dotenv.config();
-const app = express();
+connectDB();
 
-// Middleware
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/api/auth", authRoutes);
-app.use("/api", uploadRoutes);
-
-// Connect to MongoDB and start server
-connectDB();
+// Sample route
+app.get("/", (req, res) => {
+  res.send("API is working 👨‍💻");
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
